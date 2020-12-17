a7b0e71a99304d13835205d8f348ba516d96c84e
Lara Samuelson <lara.samuelson@gmail.com>
2020-12-17 14:22:30 -0800

    update

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '5432',
-    user: dunder_mifflin,
+    host : 'ec2-34-194-198-238.compute-1.amazonaws.com',
+    user: vufbbewpmyonhc,
+    database : 'dbd7ubn31e0h66',
+    password : 'ec97d4883f6a07f44ed844e19ab119e57fedb075c0108c291813d86a1e34ad37',
     database : 'noteful',
     ssl: true
   }
